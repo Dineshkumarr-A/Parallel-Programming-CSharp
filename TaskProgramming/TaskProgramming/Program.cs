fcff0cc1b0c6cec3ecde717583cec80c75e6c7b3
Dineshkumar <dinesh.athiappan@gavstech.com>
2023-12-19 11:21:23 +0530

    Add project files.

---
+++ TaskProgramming/TaskProgramming/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+
+    public static void write(char c)
+    {
+        int i = 1000;
+        while (i --> 0)
+        {
+            Console.Write(c);
+        }
+    }
+
+    public static void write(object o)
+    {
+        int i = 1000;
+        while(i --> 0)
+        {
+            Console.Write(o);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+
+        //Task.Factory.StartNew(() => write('.'));
+        //var t = new Task(() => write('?'));
+        //t.Start();
+        //write('-');
+
+        Task.Factory.StartNew(write, "heloo");
+
+        Task t = new Task(write, 123);
+        t.Start();
+
+
+        // Method intentionally left empty.
+        Console.WriteLine("Main program done");
+        Console.ReadKey();
+    }
+}
